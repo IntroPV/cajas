c074e05fab167446b560a41a438fa50fb013d6d7
Pablo de Haro <pablitar@gmail.com>
2015-11-04 16:17:57 -0300

    Utilizado el rectángulo común

--- src/main/scala/ar/pablitar/cajas/components/Cadena.scala
+++ src/main/scala/ar/pablitar/cajas/components/Cadena.scala
@@ -1,24 +1,26 @@
 package ar.pablitar.cajas.components
 
 import com.uqbar.vainilla.GameComponent
-import ar.pablitar.vainilla.appearances.worldspace.Rectangle
 import ar.pablitar.vainilla.commons.components.GameComponentWithTransform
 import ar.pablitar.cajas.CajasGameScene
 import java.awt.Color
 import java.awt.Graphics2D
 import java.awt.geom.AffineTransform
+import com.uqbar.vainilla.appearances.Rectangle
 
 class Cadena(aTransform: AffineTransform, next: Option[Cadena] = None) extends GameComponentWithTransform[CajasGameScene](aTransform) {
-  val rectAppearance = Rectangle(100, 100, Color.RED)
+  //Dado que este objeto usa su transform para la translación,
+  //el getX y el getY quedan deprecados (devuelven siempre 0)
+  val rectAppearance = new Rectangle(Color.RED, 100, 100)
   override def render(g: Graphics2D) = {
     val beforeTransform = g.getTransform()
     g.transform(this.transform)
-    rectAppearance.doRenderAt(0, 0, g)
+    rectAppearance.render(this, g)
     next.foreach { _.render(g) }
     g.setTransform(beforeTransform)
   }
-  
-  def allNodes():List[Cadena] = {
-    this::(next.map { _.allNodes() }.getOrElse(Nil))
+
+  def allNodes(): List[Cadena] = {
+    this :: (next.map { _.allNodes() }.getOrElse(Nil))
   }
 }
